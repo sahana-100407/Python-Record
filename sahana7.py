43aece5c548d2dfd704ad2fa1c4cdab3277cdd2a
sahana-100407 <11249h002@kanchiuniv.ac.in>
2025-11-10 15:13:38 +0530

    Add files via upload

---
+++ sahana7.py
@@ -0,0 +1,7 @@
+from collections import deque
+d = deque([1, 2, 3])
+print(d)
+d.appendleft(0)
+print(d)
+d.pop()
+print(d)
---
+++ sahana9.py
@@ -0,0 +1,4 @@
+from collections import namedtuple
+Point = namedtuple('Point', ['x', 'y'])
+p = Point(10, 20)
+print(p.x)  # 10
